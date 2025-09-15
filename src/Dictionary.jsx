4d6cd5a867e9b5c0aa5cf63537ab4777b8f5b707
LyviaAlam <154422742+LyviaAlam@users.noreply.github.com>
2025-09-15 17:43:27 +1000

    installed Axios and added Shecodes API

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   const [word, setWord] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
   function handleWordInput(event) {
     setWord(event.target.value);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching ${word}`);
+    // documentation : https://www.shecodes.io/learn/apis/dictionary
+    const apiKey = "143af7fd5b08cab06a8bf5bo4f3btde9";
+    const apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   return (
